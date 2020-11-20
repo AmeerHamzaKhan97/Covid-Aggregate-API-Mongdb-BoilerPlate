e8aa4cb17e59f285d04c4819e2218400b52eafbf
Nightrider0098 <hitesh08011999@gmail.com>
2020-11-20 17:23:51 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const port = 3000
-app.use(express.urlencoded());
+const port = 8080
 
 // Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const { connection } = require('./connector')
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(bodyParser.json())
-// your code goes here
+
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
